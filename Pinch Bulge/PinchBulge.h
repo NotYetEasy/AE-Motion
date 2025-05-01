fa57ba2ebf94549e24e4ec65c3b882369b7f9b31
DKT <demonkt9002@gmail.com>
2025-05-02 00:41:15 +0300

    Pinch Bulge (GPU Acceleration)

--- Pinch Bulge/PinchBulge.h
+++ Pinch Bulge/PinchBulge.h
@@ -1,49 +1,74 @@
-/*
-    PinchBulge.h
-*/
-
 #pragma once
+#ifndef PinchBulge_H
+#define PinchBulge_H
 
-#ifndef PINCHBULGE_H
-#define PINCHBULGE_H
-
-typedef unsigned char        u_char;
-typedef unsigned short        u_short;
-typedef unsigned short        u_int16;
-typedef unsigned long        u_long;
-typedef short int            int16;
-#define PF_TABLE_BITS    12
-#define PF_TABLE_SZ_16    4096
-
-#define PF_DEEP_COLOR_AWARE 1    // make sure we get 16bpc pixels; 
-// AE_Effect.h checks for this.
-
+#include "PinchBulgeKernel.cl.h"
 #include "AEConfig.h"
+#include "entry.h"
+#include "AEFX_SuiteHelper.h"
+#include "PrSDKAESupport.h"
+#include "AE_Effect.h"
+#include "AE_EffectCB.h"
+#include "AE_EffectCBSuites.h"
+#include "AE_EffectGPUSuites.h"
+#include "AE_Macros.h"
+#include "AEGP_SuiteHandler.h"
+#include "String_Utils.h"
+#include "Param_Utils.h"
+#include "Smart_Utils.h"
+
+
+#if _WIN32
+#include <CL/cl.h>
+#define HAS_HLSL 1
+#define HAS_METAL 0
+#else
+#include <OpenCL/cl.h>
+#define HAS_HLSL 0
+#define HAS_METAL 1
+#include <Metal/Metal.h>
+#include "SDK_Invert_ProcAmp_Kernel.metal.h"
+#endif
+#include <math.h>
 
 #ifdef AE_OS_WIN
-typedef unsigned short PixelType;
 #include <Windows.h>
 #endif
 
-#include "entry.h"
-#include "AE_Effect.h"
-#include "AE_EffectCB.h"
-#include "AE_Macros.h"
-#include "Param_Utils.h"
-#include "AE_EffectCBSuites.h"
-#include "String_Utils.h"
-#include "AE_GeneralPlug.h"
-#include "AEFX_ChannelDepthTpl.h"
-#include "AEGP_SuiteHandler.h"
+#define DESCRIPTION	"\nCopyright 2018-2023 Adobe Inc.\rSample PinchBulge GPU effect."
 
-/* Versioning information */
-#define    MAJOR_VERSION    1
-#define    MINOR_VERSION    0
-#define    BUG_VERSION        0
-#define    STAGE_VERSION    PF_Stage_DEVELOP
-#define    BUILD_VERSION    1
+#define NAME			"PinchBulge_GPU"
+#define	MAJOR_VERSION	1
+#define	MINOR_VERSION	0
+#define	BUG_VERSION		0
+#define	STAGE_VERSION	PF_Stage_DEVELOP
+#define	BUILD_VERSION	1
 
-/* Parameter defaults */
+// Parameter indices
+enum {
+	PINCH_INPUT = 0,
+	PINCH_CENTER,
+	PINCH_STRENGTH,
+	PINCH_RADIUS,
+	PINCH_TILES_GROUP,    // Group start
+	PINCH_X_TILES,
+	PINCH_Y_TILES,
+	PINCH_MIRROR,
+	PINCH_TILES_GROUP_END, // Group end
+	PINCH_NUM_PARAMS
+};
+
+// Parameter disk IDs
+enum {
+	CENTER_DISK_ID = 1,
+	STRENGTH_DISK_ID,
+	RADIUS_DISK_ID,
+	X_TILES_DISK_ID,
+	Y_TILES_DISK_ID,
+	MIRROR_DISK_ID
+};
+
+// Parameter defaults
 #define    PINCH_CENTER_X_DFLT    0
 #define    PINCH_CENTER_Y_DFLT    0
 #define    PINCH_STRENGTH_MIN     -1.00
@@ -53,74 +78,65 @@ typedef unsigned short PixelType;
 #define    PINCH_RADIUS_MAX       2.5
 #define    PINCH_RADIUS_DFLT      0.3
 
-/* Parameter indices */
-enum {
-    PINCH_INPUT = 0,
-    PINCH_CENTER,
-    PINCH_STRENGTH,
-    PINCH_RADIUS,
-    PINCH_NUM_PARAMS
-};
-
-/* Parameter disk IDs */
-enum {
-    CENTER_DISK_ID = 1,
-    STRENGTH_DISK_ID,
-    RADIUS_DISK_ID
-};
-
-/* String IDs */
-enum {
-    StrID_NONE,
-    StrID_Name,
-    StrID_Description,
-    StrID_Center_Param_Name,
-    StrID_Strength_Param_Name,
-    StrID_Radius_Param_Name,
-    StrID_NUMTYPES
-};
-
-/* String table */
-#define STR(id) (GetStringPtr(id))
-
-/* Custom data structure for effect */
-typedef struct PinchInfo {
-    PF_FpLong            strength;
-    PF_FpLong            radius;
-    PF_Fixed             center_x;
-    PF_Fixed             center_y;
-    PF_EffectWorld* input;
-} PinchInfo, * PinchInfoP, ** PinchInfoH;
-
-// Add this to PinchBulge.h
-typedef struct {
-    PF_EffectWorld* input;
-    PF_EffectWorld* output;
-    float strength;
-    float radius;
-    float centerX;
-    float centerY;
-    int startY;
-    int endY;
-} ThreadData;
-
-// Function declarations
-void ProcessFloatPixelsThreaded(void* refcon, A_long thread_index, A_long thread_count, A_long y_start, A_long y_stop);
-
-
-/* Function prototypes */
-char* GetStringPtr(int strNum);
-
 extern "C" {
-    DllExport
-        PF_Err
-        EffectMain(
-            PF_Cmd            cmd,
-            PF_InData* in_data,
-            PF_OutData* out_data,
-            PF_ParamDef* params[],
-            PF_LayerDef* output,
-            void* extra);
+
+	DllExport
+		PF_Err
+		EffectMain(
+			PF_Cmd			cmd,
+			PF_InData* in_data,
+			PF_OutData* out_data,
+			PF_ParamDef* params[],
+			PF_LayerDef* output,
+			void* extra);
+
 }
 
-#endif // PINCHBULGE_H
+#if HAS_METAL
+/*
+ ** Plugins must not rely on a host autorelease pool.
+ ** Create a pool if autorelease is used, or Cocoa convention calls, such as Metal, might internally autorelease.
+ */
+struct ScopedAutoreleasePool
+{
+	ScopedAutoreleasePool()
+		: mPool([[NSAutoreleasePool alloc]init] )
+	{
+	}
+
+	~ScopedAutoreleasePool()
+	{
+		[mPool release] ;
+	}
+
+	NSAutoreleasePool* mPool;
+};
+#endif 
+
+// Structure for CPU implementation
+struct PinchInfo {
+	PF_InData* in_data;
+	PF_FpLong center_x;
+	PF_FpLong center_y;
+	float strength;
+	float radius;
+	bool x_tiles;
+	bool y_tiles;
+	bool mirror;
+	PF_EffectWorld* input;
+};
+
+
+// Custom data structure for our effect parameters
+typedef struct
+{
+	float mCenterX;
+	float mCenterY;
+	float mStrength;
+	float mRadius;
+	int mXTiles;
+	int mYTiles;
+	int mMirror;
+} PinchBulgeParams;
+
+#endif
---
+++ Pinch Bulge/PinchBulgeKernel.chlsl
@@ -0,0 +1,1 @@
+#include "PinchBulgeKernel.cu"
